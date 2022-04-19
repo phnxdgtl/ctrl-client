1c6d28d8aed1146c2c684b19e0cd4437ff5e4414
Chris Gibson <chris@phoenixdigital.agency>
2022-04-19 15:23:28 +0100

    Hide delete button if empty

--- src/views/row-buttons.blade.php
+++ src/views/row-buttons.blade.php
@@ -1,10 +1,12 @@
 <div style="white-space: nowrap">
     @foreach ($row_buttons as $row_button)
-        <a  href="{{ $row_button['link'] }}"
-            class="btn btn-sm btn-{{ $row_button['class'] }}"
-            @if (!empty($row_button['rel']))
-                rel="{{ $row_button['rel'] }}"
-            @endif
-        >{!! $row_button['label'] !!}</a>
+        @if (!empty($row_button)) 
+            <a  href="{{ $row_button['link'] }}"
+                class="btn btn-sm btn-{{ $row_button['class'] }}"
+                @if (!empty($row_button['rel']))
+                    rel="{{ $row_button['rel'] }}"
+                @endif
+            >{!! $row_button['label'] !!}</a>
+        @endif
     @endforeach
 </div>
